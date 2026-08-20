da43c9492c6fb8e03ebb56187aeaeae34b0c5c12
Akash <akashsagar.ind@classichome.com>
2026-08-20 14:48:21 +0530

    change admin client initilaization

--- app/services/netsuite/orderSync.service.js
+++ app/services/netsuite/orderSync.service.js
@@ -2,7 +2,7 @@ import prisma from "../../db.server";
 import { netsuite } from "./netsuite.server";
 import { findCompanyByShopifyId } from "./company.service";
 import { findItemBySku, } from "./inventory.service";
-import { getAdminClient } from "../shopify.server";
+import { getAdminClient } from "../../shopify.server";
 import { NETSUITE_CONFIG } from "../../constants/integrationConfig";
 
 const { salesOrder: NETSUITE_SALES_ORDER } = NETSUITE_CONFIG;
@@ -40,7 +40,7 @@ function buildShopifyShippingAddress(shopifyAddress) {
 export async function processShopifyOrder(orderSyncId, options = {}) {
   let payload = null;
   const admin = await getAdminClient(process.env.SHOP);
-  
+
   try {
   const sync = await prisma.orderSync.findUnique({
     where: {
